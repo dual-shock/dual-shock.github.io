f099641de98f417c3a809354525bfe980ee851a2
dual-shock <siegemannsb@gmail.com>
2023-02-01 15:33:44 +0100

    website changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-let burger
 
-burger = document.getElementById("burger")
- 
-burger.addEventListener("click", function(){
-    
-})
-
+function toggleDropdownShow(){
+    document.getElementById("dropdown").classList.toggle("show-dropdown")
+    console.log("toggled dropdown")
+}
+window.onclick = function(event) {
+    if (!event.target.matches(".burger, #dropdown-container, #burgersvgpath")){
+        console.log("somewhere not burger clicked, hiding dropdown content")
+        console.log(event.target)
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i=0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show-dropdown")) {
+                openDropdown.classList.remove("show-dropdown")
+            }
+        }
+    }
+}
 
