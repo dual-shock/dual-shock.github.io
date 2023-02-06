0f2efa1931c8223282ced18fd188146bf66d6a5e
dual-shock <siegemannsb@gmail.com>
2023-02-06 16:58:22 +0100

    portfolio changes

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ let loadImages = true
 
 window.onclick = function(event) {
     if (!event.target.matches(".burger, #dropdown-container, #burgersvgpath")){
-        console.log("somewhere not burger clicked, hiding dropdown content")
-        console.log(event.target)
+        //! console.log("somewhere not burger clicked, hiding dropdown content")
+        //! console.log(event.target)
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i=0; i < dropdowns.length; i++) {
@@ -17,7 +17,7 @@ window.onclick = function(event) {
 }
 function toggleDropdownShow(){
     document.getElementById("dropdown").classList.toggle("show-dropdown")
-    console.log("toggled dropdown")
+    //! console.log("toggled dropdown")
 }
 
 let gallery = document.getElementById("gallery-list")
@@ -27,14 +27,14 @@ fetch("https://api.github.com/repos/dual-shock/dual-shock.github.io/git/trees/ma
     .then((response) => response.json())
     .then(data => {
         trees = data.tree
-        console.log(data.tree)
+        //! console.log(data.tree)
         let gallery = document.getElementById("gallery-list")
         let portfolio = document.getElementById("")
 
         for(let i=0;i<trees.length;i++){
             tree = trees[i]
             if(tree.path.substring(0,'imgs/gallery/'.length)=='imgs/gallery/'){
-                console.log("tree",i,"is img")
+                //! console.log("tree",i,"is img")
                 let galleryImage = `
                 <li>
                 <a href="./gallery/gallery.html">
--- testing/folders.js
+++ testing/folders.js
@@ -12,9 +12,9 @@ let folders = [
     {path:"portfolio/python-dev/converters/layer/con3.pk",
     type:"blob"},
     {path:"portfolio/python-dev/traffsys",
-    type:"blob"},
+    type:"tree"},
     {path:"portfolio/python-dev/traffsys/folder",
-    type:"blob"},
+    type:"tree"},
     {path:"portfolio/python-dev/traffsys/folder/file.py",
     type:"blob"},
 
@@ -33,53 +33,162 @@ let folders = [
     type:"blob"}
 ]
 
+function filepathsToHierarchy(listOfPaths){
+
+    let containerList = document.createElement('ul')
+    containerList.id = "myUL"
+
+    let elementObjs = [{
+        elm: containerList,
+        id: "containerUL"
+    }]
+
+    let parent = {
+        elm: containerList,
+        id: "containerUL"
+    }
+
+    let file, filePath, id, parentId, nestedList, titleSpan, child
+
+    for(let i = 0; i < listOfPaths.length; i++){
+
+        file = listOfPaths[i]
+        filePath = file.path.split('/')
+
+        id = filePath.slice(0,filePath.length).join('')
+        parentId = filePath.slice(0,filePath.length-1).join('')
+
+        
+
+        if(file.type=="tree"){
+            folder = {
+                elm: document.createElement('li'),
+                path: filePath,
+                id: id,
+                parentId: parentId
+            }
+            child = folder
+
+            nestedList = document.createElement('ul')
+            nestedList.className = "nested"
+            titleSpan = document.createElement('span')
+            titleSpan.className = "caret"
+
+            titleSpan.innerHTML = filePath[filePath.length - 1]
+
+            folder.elm.appendChild(titleSpan)
+            folder.elm.appendChild(nestedList)
+
+            elementObjs.push(folder)
+
+        } else {
+            file = {
+                elm: document.createElement('li'),
+                path: filePath,
+                id: id,
+                parentId: parentId
+            }
+            child = file
+
+            file.elm.innerHTML = filePath[filePath.length - 1]
+            elementObjs.push(file)
+        }
 
 
-let portfolio = document.createElement('ul')
-portfolio.id = 'myUL'
+
+        if(!i){continue}
+        
+        console.log(i)
+        parentElm = elementObjs.find(item => item.id == child.parentId).elm
+        
+        if(parentElm.children[1] != undefined){
+            parentElm = parentElm.children[1]
+        }
+
+        console.log(child.elm.outerHTML, parentElm.outerHTML)
 
 
 
-let elements = []
+    }
+    console.log(elementObjs)
+}
+
+
+let portfolioElm = document.createElement('ul')
+portfolioElm.id = 'myTESTINGUL'
+
+let elements = [{
+    elm: portfolioElm,
+    id: "portfolio"
+}]
+let item, itemPath, folder, folderPath, nested, 
+    folderName, file, filePath, id, parentId, 
+    parentElm, childElm, testparentElm
 
 for(let i=0;i<folders.length;i++){
+    item = folders[i]
+    itemPath = item.path.split('/')
 
-    let item = folders[i]
-    let itemPath = item.path.split('/')
+    id = itemPath.slice(0,itemPath.length).join('')
+    parentId = itemPath.slice(0,itemPath.length-1).join('')
 
     if(item.type=='tree'){
-        //console.log(item.path.split('/'))
-        let folderPath = itemPath
-        let folder = document.createElement('li')
-        
-        let folderName = document.createElement('span')
+        folderPath = itemPath
+        folder = {
+            elm: document.createElement('li'),
+            path: itemPath,
+            id: id,
+            parentId: parentId
+        }
+        nested = document.createElement('ul')
+        nested.className = "nested" 
+        folderName = document.createElement('span')
         folderName.className = 'caret'
+
         folderName.innerHTML = folderPath[folderPath.length - 1]
-        folder.appendChild(folderName)
+        folder.elm.appendChild(folderName)
+        folder.elm.appendChild(nested)
         elements.push(folder)
-        //console.log(folder.outerHTML)
-        console.log(folderPath)
     }
     else{
-        let filePath = itemPath
-        let file = document.createElement('li')
-        file.innerHTML = filePath[filePath.length - 1]
+        filePath = itemPath
+        file = {
+            elm: document.createElement('li'),
+            path: itemPath,
+            id: id,
+            parentId: parentId
+        }
+        file.elm.innerHTML = filePath[filePath.length - 1]
         elements.push(file)
-        //console.log(file.outerHTML)
-        console.log(filePath)
     }
-
-
 }
 
-console.log(portfolio.outerHTML)
-for(let i=0;i<elements.length;i++){
-    console.log(elements[i].outerHTML)
+
+console.log(elements)
+
+for(let i = 1; i < elements.length; i++){
+    childElm = elements[i].elm
+    console.log("\n\n")
+
+    console.log(elements[i].elm.outerHTML)
+    console.log(elements[i].id,elements[i].parentId)
+
+    parentElm = elements.find(item => item.id == elements[i].parentId).elm
+
+    console.log("parent: ", parentElm.outerHTML)
+    if(parentElm.children[1]!=undefined){
+        parentElm = parentElm.children[1]
+    }
+    
+     
+    parentElm.appendChild(childElm)
+    console.log(portfolioElm)
 }
+
+
 
 
 let testFolio = document.getElementById("myUL")
-console.log(testFolio.outerHTML)
-
+console.log(testFolio)
 
 
