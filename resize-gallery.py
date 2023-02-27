eccf54c04db329233e4ccd30d08bc09d9dcf222a
dual-shock <siegemannsb@gmail.com>
2023-02-27 18:11:09 +0100

    fixed wrong image rotation from py script

--- resize-gallery.py
+++ resize-gallery.py
@@ -1,6 +1,6 @@
 import os
 
-from PIL import Image
+from PIL import Image, ImageOps
 
 cur_dir = __file__.rpartition('\\')[0]
 
@@ -11,6 +11,7 @@ for img in list_of_imgs:
     if '.' in img and img not in list_of_thumbs:
         print(img)
         image = Image.open(f'{cur_dir}\\imgs\\gallery\\{img}')
+        image = ImageOps.exif_transpose(image)
         image.thumbnail((200,200))
         image.save(f'{cur_dir}\\imgs\\gallery\\thumbs\\{img}')
 
