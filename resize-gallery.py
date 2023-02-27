aaf458ad38399f7156f1ca1f3a9917f38941c17c
dual-shock <siegemannsb@gmail.com>
2023-02-27 14:28:32 +0100

    added thumbs for optimized loading

--- resize-gallery.py
+++ resize-gallery.py
@@ -5,11 +5,13 @@ from PIL import Image
 cur_dir = __file__.rpartition('\\')[0]
 
 list_of_imgs = os.listdir(f'{cur_dir}\\imgs\\gallery')
+list_of_thumbs = os.listdir(f'{cur_dir}\\imgs\\gallery\\thumbs')
 
 for img in list_of_imgs:
-    if '_thumb' not in img:
+    if '.' in img and img not in list_of_thumbs:
+        print(img)
         image = Image.open(f'{cur_dir}\\imgs\\gallery\\{img}')
         image.thumbnail((200,200))
-        image.save(f'{cur_dir}\\imgs\\gallery\\{img[:-len(os.path.splitext(img)[1])]}_thumb{os.path.splitext(img)[1]}')
+        image.save(f'{cur_dir}\\imgs\\gallery\\thumbs\\{img}')
 
 
