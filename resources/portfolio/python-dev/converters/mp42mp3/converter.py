4aba7954e8145097dc9ee294c63842a4f1054cf8
dual-shock <siegemannsb@gmail.com>
2023-10-11 20:32:09 +0200

    del desktop local

--- resources/portfolio/python-dev/converters/mp42mp3/converter.py
+++ resources/portfolio/python-dev/converters/mp42mp3/converter.py
@@ -1,5 +1,5 @@
 import moviepy.editor as mp
 from pathlib import Path
 
-video = mp.VideoFileClip("S:/SunnyVids/WhatsApp Video 2023-09-26 at 22.08.04.mp4")
-video.audio.write_audiofile("C:/Users/nokken/Desktop/LocalSongs/Let You Break My Heart Again.mp3")
+video = mp.VideoFileClip("C:/Users/nokken/Desktop/LocalSongs/WhatsApp Audio 2023-10-04 at 23.35.16.mp4")
+video.audio.write_audiofile("C:/Users/nokken/Desktop/LocalSongs/Bag Of Bones.mp3")
