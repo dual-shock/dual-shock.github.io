3902177b886c749d93e6342777e44eee46be2aee
dual-shock <siegemannsb@gmail.com>
2024-03-01 21:20:08 +0100

    trying last fm

--- js/main.js
+++ js/main.js
@@ -138,7 +138,96 @@ function parallaxHandler(){
 
 
 
+// ? This entire block i stole from github.com/DragonWife
 
+
+const LASTFM_API_KEY = "8b8e23a1e50a3e03a82bc10dbc293f27"
+const username = "Offdeezboy" // change username here
+const url = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=json&api_key=" + LASTFM_API_KEY + "&limit=1&user=" + username
+
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+	xmlHttp.send(null);
+	return xmlHttp.responseText;
+}
+function relativeTime(time, time_text) {
+    var time_now = Math.round(Date.now() / 1000)
+    var time_diff = time_now - time
+
+    let SEC_IN_MIN = 60
+    let SEC_IN_HOUR = SEC_IN_MIN * 60
+    let SEC_IN_DAY = SEC_IN_HOUR * 24
+
+    if (time_diff < SEC_IN_HOUR) {
+        let minutes = Math.round(time_diff / SEC_IN_MIN)
+        return minutes + " minute" +
+            ((minutes != 1) ? "s" : "") + " ago"
+    }
+    if (time_diff >= SEC_IN_HOUR && time_diff < SEC_IN_DAY) {
+        let hours = Math.round(time_diff / SEC_IN_HOUR)
+        return hours + " hour" +
+            ((hours != 1) ? "s" : "") + " ago"
+    }
+    if (time_diff >= SEC_IN_DAY)
+    console.log(time_text)
+        return time_text
+}
+
+function updateLastFmData(){
+    var json = JSON.parse(httpGet(url));
+    var last_track = json.recenttracks.track[0]
+    var track = last_track.name
+    var trackLink = last_track.url
+    var artist = last_track.artist['#text']
+    let relative_time = null
+    
+    if (last_track.date) {
+        var unix_date = last_track.date.uts
+        var date_text = last_track.date["#text"]
+        relative_time = relativeTime(unix_date, date_text)
+    }
+    var now_playing = (last_track["@attr"] == undefined) ? false : true
+    var imageLink = last_track.image[1]["#text"]
+    document.getElementById("listening-img").src = imageLink
+    document.getElementById("listening-title").innerHTML = `<p>${track}</p>`
+
+    document.getElementById("listening-artist").innerHTML = artist
+    console.log(now_playing, last_track.date)
+    console.log(now_playing)
+    if(now_playing){
+        document.getElementById("listening-time").innerHTML = `
+        <img width="60px" height="60px" src="./imgs/headset.gif" alt="">
+        <div id="listening-now">Listening now! </div>
+        `
+    }
+    else{
+        document.getElementById("listening-time").innerHTML = `
+        <img width="60px" height="60px" src="./imgs/sleeping.gif" alt="">
+        <div id="listening-now">${relative_time} </div>
+        `
+    }
+    console.log(imageLink)
+    console.log(
+        "Artist: " + artist + "\n" +
+        "Track: " + track + "\n" +
+        "Date: " + relative_time + "\n" +
+        "Now playing: " + now_playing)
+}
+updateLastFmData()
+let checkSong = setInterval(updateLastFmData,10000);
+
+
+
+
+
+// ? End of block
+
+
+
+
+
+if(false){
 let promises = []
 let reposToInclude = [
     "journal",
@@ -146,6 +235,7 @@ let reposToInclude = [
     "dual-shock.github.io",
     "uni.todo"
 ]
+
 
 
 async function loadPortfolioLinks(){
@@ -156,6 +246,8 @@ async function loadPortfolioLinks(){
         )
     }
 }
+
+
 
 try {
     loadPortfolioLinks();
@@ -215,4 +307,5 @@ else{
         this.classList.toggle("caret-down");
         });
     }
+}
 }
